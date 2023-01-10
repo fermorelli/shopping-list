9361a66d1943851960bc6d4647de26ef0f77956f
fermorelli <fer.cmorelli@gmail.com>
2023-01-10 18:02:08 -0300

    add method and list displayed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const alert = document.querySelector('.alert');
-const form = document.querySelector('.grocery-form');
+const form = document.querySelector('.shopping-form');
 const grocery = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
@@ -10,24 +10,47 @@ let editElement;
 let editFlag = false;
 let editId = '';
 
-form.addEventListener('submit', addItem)
-
-const addItem = (e)=>{
+const addItem = (e) => {
     e.preventDefault();
     const value = grocery.value;
 
     //give items unique id using miliseconds date
     const id= new Date().getTime().toString();
 
-    if(value !== '' && editFlag === false){
-        console.log('add item to the list')
+    if(value && !editFlag){
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = ` <p class="title">${value}</p>
+        <div class="btn-container">
+          <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button type="button" class="delete-btn">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div>`;
+        list.appendChild(element);
+        displayAlert('item added to list', 'success');
+        container.classList.add('show-container');
     }
-    else if(value !== '' && editFlag === true){
+    else if(value && editFlag){
         console.log('editing')
     }
     else{
-        console.log('empty value')
+        displayAlert('please enter a value', 'danger')
     }
-
-    localStorage.setItem('grocery', value);
 }
+
+const displayAlert = (text, action) => {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    setTimeout(()=>{
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    },2000)
+}
+
+form.addEventListener("submit", addItem);
